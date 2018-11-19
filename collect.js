ee0e9f36cb76dd507c325d249e3da1482459a09e
Jon Anderson <jon.david.anderson@gmail.com>
2018-11-18 18:51:56 -0800

    Removed duplicated code. Added JSON stringify.

--- collect.js
+++ collect.js
@@ -193,76 +193,7 @@ let rowsAndDetails = $.when(...reqs)
             }
         })
 
-        log(outdata)
-
-        cols = [
-            'id',
-            'date',
-            'hour',
-            'author',
-            'link',
-            'type',
-            'subject',
-            'name',
-            'shout',
-            'text',
-            'keywords'
-        ]
-
-        csv = outdata.reduce((csv, row) =>
-                  csv + '\n'
-                      + cols.map((name) =>
-                            row[name]
-                        ).join('|')
-              , cols.join('|'))
-
-        log(csv)
-    })
-    .fail(function() {
-        throw "failed to load details"
-    })
-
-    .done(function() {
-        log("loaded details")
-
-        let details =
-            Array.from(arguments)
-                 .map((result) => result[0])
-
-        if ($rows.length != details.length) {
-            log('rows: ' + $rows.length +
-                ', details: ' + details.length
-            )
-            throw 'WARN! rows and details arent one to one'
-        }
-
-        let rowsAndDetails =
-            details.map((detail, i) =>
-                [$($rows[i]), $(detail)]
-            )
-
-        log(rowsAndDetails)
-
-        let outdata = rowsAndDetails.map((rad) => {
-            let $row = rad[0]
-            let $details = rad[1]
-
-            return {
-                id: id($row),
-                date: date($row),
-                hour: hour($row),
-                author: author($row),
-                link: link($details),
-                type: type($details),
-                subject: subject($details),
-                name: aname($details),
-                shout: shout($details),
-                text: text($details),
-                keywords: keywords($details)
-            }
-        })
-
-        log(outdata)
+        log(JSON.stringify(outdata))
 
         cols = [
             'id',
